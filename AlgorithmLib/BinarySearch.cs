95f93c7e87c89ce7c358626514d92d1d1f5041a7
ColtonPritchett <121995308+ColtonPritchett@users.noreply.github.com>
2024-01-19 11:41:09 -0700

    finished assignment 1

--- AlgorithmLib/BinarySearch.cs
+++ AlgorithmLib/BinarySearch.cs
@@ -4,7 +4,7 @@ public static class BinarySearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
+        
         return -1;
     }
 }
